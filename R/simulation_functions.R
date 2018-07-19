0561a1383fcaa0d6779644acecd63f8f92db7ebb
Leah Kumle <leahkumle@gmail.com>
2018-07-19 14:27:13 +0200

    changed name of function

---
+++ R/simulation_functions.R
@@ -0,0 +1,336 @@
+# simulation stuff
+
+
+
+#-------------------------------------------------------------------------------------------------------#
+# FULL POWER PROCESS
+#### mixed_power
+
+#' Function to prepare a mixed model with varied fixed effect estimantes
+#'
+#' \code{prepare_rnorm_model()} changes the fixed effect estimates of the
+#' specified model. It randomly selects "new" fixed effect estimates out of
+#' a normal distribution around the "old" estimates. Non significant effects
+#' in the given model are ignored and no rnorm-value is computet for them.
+#'
+#' @param model mixed model of interest
+#' @param data_emp pilot data that fits the mixed model of interest
+#' @param subvar name of the variable that contains the subject´s number
+#' in data_emp
+#' @param critical_value z/t value to test if a given fixed effect
+#' is significant
+#'
+#' @return A modified mixed model
+#'
+#' @export
+fullpower <- function(model_emp, data_emp, subvar, fixed_effects,
+                      critical_value, sample_sizes, n_sim,
+                      databased = T, safeguard = T, rnorm = T){
+
+  #### This function combines the whole power simulation process ###
+
+  # prepare storing output
+  output <- list()
+  i <- 1
+
+  # ------------------------ #
+  # 1. databased
+  if (databased == T){
+    databased_power_values <- power_simulation(model_emp, data_emp, subvar, fixed_effects,
+                                               critical_value, sample_sizes, n_sim,
+                                               safeguard = F, rnorm = F)
+
+    # store output
+    databased_power_values["mode"] <- "databased"
+    databased_power_values["effect"] <- row.names(databased_power_values)
+
+    output[[i]] <- databased_power_values
+    i <- i + 1 # increment i so the next output gets stored at the right position
+
+  } # end if databased
+
+  # 2. safeguard
+  if (safeguard == T){
+    safeguard_power_values <- power_simulation(model_emp, data_emp, subvar, fixed_effects,
+                                               critical_value, sample_sizes, n_sim,
+                                               safeguard = T, rnorm = F)
+
+    safeguard_power_values["mode"] <- "safeguard"
+    safeguard_power_values["effect"] <- row.names(safeguard_power_values)
+    output[[i]] <- safeguard_power_values
+    i <- i + 1 # increment i so the next output gets stored at the right position
+
+  } # end if safeguard
+
+
+  # 3. rnorm
+  if (rnorm == T){
+    rnorm_power_values <- power_simulation(model_emp, data_emp, subvar, fixed_effects,
+                                           critical_value, sample_sizes, n_sim,
+                                           safeguard = F, rnorm = T)
+
+    rnorm_power_values["mode"] <- "rnorm"
+    rnorm_power_values["effect"] <- row.names(rnorm_power_values)
+    output[[i]] <- rnorm_power_values
+
+  } # end if rnorm
+  # ------------------------ #
+
+  # prepare storing final results
+  results <- data.frame()
+
+  # combine results
+  for (ii in 1:length(output)){
+    results <- rbind(results, output[[ii]])
+  }# end for loop
+
+  # return and save stuff
+  save(results, file = "output_powersismulation.Rda")
+  results
+
+} # end function
+
+#-------------------------------------------------------------------------------------------------------#
+# POWER SIMULATION
+
+# do the whole power analysis process
+power_simulation <- function(model_emp, data_emp, subvar, fixed_effects,
+                             critical_value, sample_sizes, n_sim,
+                             safeguard = F, rnorm = F){
+
+
+  # PREPARE:
+  # get depvar from model_emp to hand it siimulateSamplesize() later
+  depvar <- get_depvar(model_emp)
+
+
+  # PREPARE TO RUN IN PARALLEL
+  cores= parallel::detectCores()
+  cl <- parallel::makeCluster(cores[1]-1) #not to overload your computer
+  doParallel::registerDoParallel(cl)
+
+  #------------------------------------#
+  #------------------------------------#
+  # IF SAFEGUARD POWER:
+  if (safeguard == T){
+    print("Hey its safeguard")
+    model_for_simulation <- prepare_safeguard_model(model_emp,confidence_level, critical_value)
+  } else {
+    print("no safeguard")
+    model_for_simulation <- model_emp
+  }
+
+  #------------------------------------#
+  #------------------------------------#
+
+
+
+  #------------------------------------#
+  # prepare storing power values
+
+  ## 1. create an empty data frame witg the right dimensions and names
+  # dimension = ncol, nrow
+  # [-1] removes the Intercept
+  n_row <- length(fixef(model_emp)[-1])
+  # n col
+  n_col <- length(sample_sizes)
+
+  # row_names = names of effects (again remove intercept)
+  row_names <- row.names(summary(model_emp)$coefficients)[-1]
+
+  # empty data frame
+  power_values_all <- data.frame(matrix(ncol = n_col, nrow = n_row), row.names = row_names)
+  # name stuff (header)
+  names(power_values_all) <- sample_sizes
+
+  index_n <- 0 # index to store power value in Power_value later
+
+  # loop through different sample sizes
+  for (n in sample_sizes){
+
+    # inform which sample sizes we are computing power right now
+    print(n)
+
+    index_n <- index_n + 1
+
+    # prepare simulation for current n
+    ## 1. create object that can store simulations --> data frame with effects as collumns and nsim rows
+    #store_simulations <- data.frame(matrix(ncol = n_row, nrow = n_sim))
+    #names(store_simulations) <- names(fixef(model_emp)[-1])
+
+    # repeat simulation n_sim times
+    # store outcome in store_simulations
+    # --> this is a list of vectors!!
+    store_simulations <- foreach::foreach(icount(n_sim), .combine = "cbind", .export=ls(envir=globalenv()),
+                                 .packages = c("lme4")) %dopar% {
+
+                                   #------------------------------------#
+                                   #------------------------------------#
+                                   # IF RNORM MODEL:
+                                   if (rnorm == T){
+                                     model_for_simulation <- prepare_rnorm_model(model_emp, data_emp, subvar)
+                                   }
+                                   #------------------------------------#
+                                   #------------------------------------#
+
+                                   #-------------------------------------#
+                                   # 1. simulate data set with n subjects
+                                   simulated_data <- simulateSamplesize(n_want = n,
+                                                                        data_emp = data_emp,
+                                                                        model_emp = model_for_simulation,
+                                                                        depvar = depvar,
+                                                                        subvar = subvar)
+
+                                   #------------------------------------#
+                                   #2. code contrasts for simulated data set
+                                   final_dataset <- reset_contrasts(simulated_data, data_emp, model_emp, fixed_effects)
+
+
+                                   #-------------------------------------#
+                                   # 3. refit model_emp to current data set (final_dataset)
+                                   # --> update model emp with new data set
+                                   model_sim <- update(model_emp, data = final_dataset)
+
+                                   #-------------------------------------#
+                                   # 4. analyze final_data set and store result
+
+
+
+                                   # check significance
+                                   # --> check_significance() returns 1 if effect is significant, 0 if not
+                                   # --> store significance in specified vector
+                                   to.store_simulations <- check_significance(model_sim, critical_value)
+
+                                 }# end for loop (n_sim)
+
+    # -------------------------------------#
+    # 5. compute power
+    ## compute power!
+    # margin = 2 --> apply FUN on columns
+    # --> vector withs names
+    power_values_n <- apply(store_simulations, MARGIN = 1, FUN = mean, na.rm = T)
+
+    # -------------------------------------#
+    # 6. store power value
+    ## store it!
+    column_name <- as.character(n)
+    power_values_all[column_name] <- power_values_n
+
+  } # end for loop (samplesizes)
+
+  ## END PARALLEL PROCESSING
+  parallel::stopCluster(cl)
+
+  # return data based power values
+  power_values_all
+
+
+} # end power simulation function
+
+
+
+
+# simulate new data sets
+simulateSamplesize <- function(n_want, data_emp, model_emp, depvar, subvar){
+  # ---------------------------------------------------------------------------- #
+  # STEP 1: set relevant paramaters
+
+  # how many subjects are in the pilot data? --> n_now
+  n_now <- get_n(data_emp, subvar)
+
+  # number of dublicates we need from the original dataset # --> ceiling()
+  # --> floor gets next lower integer (we already have one multiplication with exp4)
+  mult_factor <- ceiling(n_want/n_now)
+
+  # --> how many subjects do we need to remove if we use this multiplication factor?
+  too_much <- (n_now*mult_factor) - n_want
+
+  # --------------------------------------------------------------------------- #
+  # STEP 2: sumulate data set
+
+  # simulate (mult_factor) data sets
+  for (i in 1:mult_factor){
+
+    # simuate new data --> variable of interest
+    new_subject_data <- lme4::simulate(model_emp, nsim = 1)
+
+    ###--- create new data set: rename vp variable and replace variable of interest with simulated data--- ##
+    # copy old data set
+    new_part <- data_emp
+
+
+
+    # first iteration: only change variable of interest
+    if (i==1){
+      new_part[[depvar]] <- new_subject_data[,1]
+      final_data <- new_part
+      # from second iteration on: change subject names and rbind new data to existing data
+    } else {
+
+
+
+      # 1. increment vp variable wih current n and replce the old names
+      # --> change subjects names only from second iteration on
+
+      # check if it is a factor
+      if( is.numeric(final_data[[subvar]]) == F) {
+        # do step 1.
+
+        new_names <- as.numeric(new_part[[subvar]]) + (i-1)*n_now
+        new_part[[subvar]] <- new_names
+
+        # re-convert subvar to a factor
+        new_part[[subvar]] <- factor(new_part[[subvar]])
+      } else {
+
+        # do step 1.
+        new_names <- new_part[[subvar]] + (i-1)*n_now
+        new_part[[subvar]] <- new_names
+
+      } # end inner if else
+
+
+      # 2. replace variable of interest
+      new_part[[depvar]] <- new_subject_data[,1]
+
+      # 3. combine new and old data set
+      # --> if first iteration: nothing to rbind, so just new_part it is
+      # --> second and more iteration: rbind new simulated subjects to already simulated ones
+      final_data <- rbind(final_data, new_part)
+    } # end outer if else
+  } # end for-loop
+
+  # --------------------------------------------------------------------------- #
+  # STEP 3: delete participants to get the exact n  and return it
+
+  #--> subvar needs to be numeric for that, so it needs to be converted temporarly
+
+  # store old version
+  #store_old <- final_data[[subvar]]
+
+  # check if it is a factor
+  if( is.numeric(final_data[[subvar]]) == F) {
+    # 2. convert  to numeric
+    final_data[[subvar]] <- as.numeric(final_data[[subvar]])
+
+
+    # now do STEP 3:
+    final_data <- final_data[final_data[[subvar]] <= n_want,]
+
+    # re-convert to factor
+    final_data[[subvar]] <- factor(final_data[[subvar]])
+
+    # if not: just subset final data to correct n
+  } else {
+    final_data <- final_data[final_data[[subvar]] <= n_want,]
+  }# end if
+
+  # now do STEP 3:
+  #final_data <- final_data[final_data[[subvar]] <= n_want,]
+
+  # get back to old version
+  #final_data[[subvar]] <- store_old[1:nrow(final_data)]
+
+  # return final_data
+  final_data
+}
---
+++ R/utility_functions.R
@@ -0,0 +1,324 @@
+######## utility functions
+
+
+#-------------------------------------------------------------------------------------------------------#
+
+#' Function to prepare a mixed model with varied fixed effect estimantes
+#'
+#' \code{prepare_rnorm_model()} changes the fixed effect estimates of the
+#' specified model. It randomly selects "new" fixed effect estimates out of
+#' a normal distribution around the "old" estimates. Non significant effects
+#' in the given model are ignored and no rnorm-value is computet for them.
+#'
+#' @param model mixed model of interest
+#' @param data_emp pilot data that fits the mixed model of interest
+#' @param subvar name of the variable that contains the subject´s number
+#' in data_emp
+#' @param critical_value z/t value to test if a given fixed effect
+#' is significant
+#'
+#' @return A modified mixed model
+#'
+#' @export
+prepare_rnorm_model <- function(model_emp, data_emp, subvar, critical_value){
+
+  # --------------------------------------------------------- #
+
+  # 1. get fixed effects and their distribution
+
+  # [-1, ] removes intercept --> we dont need it
+  fixefs <- summary(model_emp)$coefficients[-1,]
+  fixef_values <- fixefs[, "Estimate"]
+
+  # SD = SE* sqrt(n)
+  # get n to compute SD
+  n <- get_n(data_emp, subvar)
+  SD <- fixefs[, "Std. Error"] * sqrt(n)
+
+  ### prepare ###
+  # --> this is a TRUE FALSE vector
+  significant_effects <- check_significance(model_emp, critical_value)
+
+
+
+  # ---------- change model_emp ------------------------ #
+  # 1. look at every effect
+  for (i in 1:length(significant_effects)){
+
+    # check if effect is significant:
+    if(significant_effects[i] == TRUE){
+
+      # change parameter to rnorm value
+      # 1. get SD of current effect
+      sd <- SD[[i]]
+
+      # get current effect (value of it)
+      effect <- fixef_values[i]
+
+      # 2. construct normal distribution and randomly choose one observation
+      # 3. change model to this random effect parameter
+      # i +1 : because intercept is still in this @beta thing
+      model_emp@beta[i + 1] <- rnorm(1, mean = effect, sd = sd)
+
+    } else if(significant_effects[i] == FALSE){
+
+      # inform user that old paramter is used to compute power
+      non_significant_effect <- row.names(summary(model_emp)$coefficients)[i+1]
+      cat(paste("WARNING:", non_significant_effect, "is not significant in model_emp. \n  rNorm Power cannot be computed for this effect\n"))
+
+    } else {
+
+      cat("WARNING: Something went wrong with the rnorm model")
+    } # end if else
+  } # end for
+
+  ### return modified model == rnorm model
+  model_emp
+} # end function
+
+
+
+#-------------------------------------------------------------------------------------------------------#
+# PREPARE SAFEGUARD POWER
+
+
+#' Function that computes confidence intervals for fixed effects and
+#' sets them to the lower boundary of this CI
+#'
+#' \code{prepare_safeguard_model()} changes the fixed effect estimates of the
+#' specified model. It computes confidence Intervals with the "Wald"- method
+#' and sets the fixed effect estimates to the lower boundary of this confidence
+#' interval. Non significant effects in the given model are ignored and
+#' no safeguard-value is computet for them.
+#'
+#' @param model_emp mixed model of interest
+#' @param confidence_level level of confidence that the parameter lies
+#' in the interval
+#' @param critical_value z/t value to test if a given fixed effect
+#' is significant
+#'
+#' @return A modified mixed model
+#'
+#' @export
+prepare_safeguard_model <- function(model_emp, confidence_level, critical_value){
+
+  # ---------- prepare ------------------------ #
+  # 1. check which effects are significant
+
+  # --> this is a TRUE FALSE vector
+  significant_effects <- check_significance(model_emp, critical_value)
+
+  # 2. compute confidence intervals
+
+  # things we want intervals for: (kick out those .sig01 stuff)
+  effects <- row.names(summary(model_emp)$coefficients)[-1]
+  confints <- confint(model_emp, method = "Wald",  level = confidence_level, parm = effects)
+
+
+  # ---------- change model_emp ------------------------ #
+  # 1. look at every effect
+  for (i in 1:length(significant_effects)){
+
+    # check if effect is significant:
+    if(significant_effects[i] == TRUE){
+
+      # change paramter to safeguard value
+
+      # A. get safeguard effect: needs to be the one closer to 0
+      #--> depends on effect of interest: negativ or positiv ?
+      row <- i +1 # because there also is the intercept in coefficients that we want to skip
+      safeguard_effect <- ifelse(summary(model_emp)$coefficients[row,1] > 0,
+                                 confints[i,1],
+                                 confints[i,2])
+
+      # B. change model parameter of effect of interest to safeguard effect
+      model_emp@beta[row] <- safeguard_effect
+
+    } else if(significant_effects[i] == FALSE){
+
+      # inform user that old paramter is used to compute power
+      non_significant_effect <- row.names(summary(model_emp)$coefficients)[i+1]
+      cat(paste("WARNING:", non_significant_effect, "is not significant in model_emp. \n  Safeguard Power cannot be computed for this effect\n"))
+
+    } else {
+
+      cat("WARNING: Something went wrong with the safeguard model")
+    } # end if else
+  } # end for
+
+
+  # ---------- return safeguard model ------------------------ #
+  model_emp
+} # end function
+
+#-------------------------------------------------------------------------------------------------------#
+# RESET CONTRASTS FUNCTION
+
+# simulateSamplesize loses contrasts --> rbind
+# --> this function resets all contrasts from data_emp
+
+
+#' Resets specified contrasts in the simulated dataset
+#'
+#' \code{reset_contrasts()} makes sure that contrasts in the simulated
+#' dataset equals the contrasts in the pilotdata used for simulation.
+#' Because the simulate_sanplesize function loses specified contrasts,
+#' they need to be re-specified.
+#' \code{reset_contrasts()} checks all contrasts on the fixed-effect-variables
+#' in the pilot dataset and copies them to the simulated dataset.
+#'
+#' @param simulated_data simulated data set (= output of simulate_samplesize)
+#' @param data_emp pilot data used for power simulation
+#' @param model_emp mixed model used for power simulation.
+#' Needs to fit data_emp.
+#' @param fixed_effects names of variables in data_emp that are used as
+#' fixed effects in model_emp
+#'
+#' @return Date frame
+#'
+#' @export
+reset_contrasts <- function(simulated_data, data_emp, model_emp, fixed_effects ) {
+
+  # --> 1. get names from all variables in model
+  variable_names <- fixed_effects
+
+
+  # --> 2. loop through names and reset contrasts for all involved variables
+  for (variable in variable_names){
+
+    # check if variable is a factor (contrasts only work with factors)
+    if (is.factor(data_emp[[variable]]) == T){
+      # get original contrast
+      data_emp_contr <- contrasts(data_emp[[variable]])
+
+
+      # apply original contrast to simulated data set
+      contrasts(simulated_data[[variable]]) <- data_emp_contr
+    } # end if
+  } # end for loop
+
+  # return simulated data set
+  simulated_data
+} # end function
+
+#-------------------------------------------------------------------------------------------------------#
+
+# CHECK SIGNIFICANCE
+
+#' Gets t/z value out of model_emp and compares it to a ctritical value
+#'
+#' \code{check_significance()} checks the t/z value of all fixed effects and
+#' compares them to the specified critical value. Returns a logical vector
+#' indicating whether one effect is significant or not. TRUE == significant,
+#' FALSE == not significant.
+#'
+#' @param model mixed model you want to check
+#' @param critical_value t/z value you want to check against
+#'
+#' @return logical vector
+#'
+#' @export
+check_significance <- function(model, critical_value){
+
+  store_significance <- c()
+
+  for (i in 1:length(fixef(model)[-1])){
+    # --------------------------------- get t/z value ---------------------- #
+    # 1. ROW
+    # -->  get row of effect of interest
+    row <- i + 1
+
+    # 2. COLUMN which column is the right one??
+    # --> check if model is lmer or glmer
+
+    # what happens if model_emp is a lmer?
+    if (lme4::getME(model, "devcomp")$dims[["GLMM"]] == 0) {
+
+
+      #... this: we need "t value"
+      t_value <- abs(coef(summary(model))[row, "t value"])
+
+
+      # ------------ test aganist significance level ------- #
+
+      # --> is either TRUE or FALSE (= 1 or 0)
+      is_significant <- (t_value >= critical_value)
+
+
+      # store significance
+      store_significance <- c(store_significance, is_significant)
+
+
+      # what happens if model_emp is a glmer?
+    } else if (lme4::getME(model, "devcomp")$dims[["GLMM"]] == 1) {
+
+      #... this: we need "z value"
+      z_value <- abs(coef(summary(model))[row, "z value"])
+
+      # ------------ test aganist significance level ------- #
+
+      # --> is either TRUE or FALSE (= 1 or 0)
+      is_significant <- (z_value >= critical_value)
+
+      # store significance
+      store_significance <- c(store_significance, is_significant)
+    } # end if
+  } # end for
+  store_significance
+} # end function
+
+#-------------------------------------------------------------------------------------------------------#
+# GET N FROM DATA EMP
+
+#' Gets number of participants in a dataset
+#'
+#' \code{get_n()} checks the column indicating the subject number and returns
+#' the number of unique occurences.
+#'
+#' @param data data set you want to get n for
+#' @param subvar name of variable in data that contains the subject number
+#'
+#' @export
+get_n <- function(data, subvar){
+
+  # --> first: check if subvar is numeric and convert to numeric if not
+  # --> max() needs numeric vector
+  # 1. check if subvar is numeric
+  if( is.numeric(data[[subvar]]) == F) {
+
+    # 2. convert  to numeric
+    data[[subvar]] <- as.numeric(as.character(data[[subvar]]))
+  } # end if
+
+  # --> now get n_now ;)
+  n <- length(unique(data[[subvar]], na.rm = T))
+
+  # return nnow
+  n
+}
+
+
+#-------------------------------------------------------------------------------------------------------#
+# GET DEPVAR FUNCTION
+
+#' Gets dependent variable of a mixed model
+#'
+#' \code{get_depvar()} checks the formula of a given mixed model and return
+#' the variable name of the dependent variable.
+#'
+#' @param model mixed model you want to know the dependent variable of
+#' @export
+get_depvar <- function(model){
+
+  # 1. extract model formula and convert it to character
+  # --> easier to extract depvar and we need it as character anyways
+  formula <- as.character(model@call$formula)
+
+  # 2. extrcat depvar --> second element. No clue why 2... but who cares!
+  depvar <- formula[2]
+
+  # return depvar
+  depvar
+
+} # end function depvar
+
